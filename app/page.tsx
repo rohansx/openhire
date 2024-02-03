06a877bee0d96dc362f28fe8bf9b847b25cae5e9
rohansx <sharmarohan615@gmail.com>
2024-02-04 00:08:03 +0530

    added routes

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+import React from "react";
 import Link from "next/link";
 
 const Home = () => {
   return (
     <div>
       <h1>Welcome to OpenHire</h1>
-      <Link href="/interview">Start Interview</Link>
+      <Link href="/Components/interview">Start Interview</Link>
     </div>
   );
 };
